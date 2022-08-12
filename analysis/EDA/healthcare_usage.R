fbfdd5b161447f7eff9cfb3c26c1353ab9b77181
Tom Wagstaff <tom.wagstaff@health.org.uk>
2022-08-12 15:56:42 +0000

    Healthcare use

--- analysis/EDA/healthcare_usage.R
+++ analysis/EDA/healthcare_usage.R
@@ -10,6 +10,29 @@ table(yr2004$anycd_ic, yr2004$contact_with_primary_care) %>% prop.table(margin =
 # so what % have a BD *and* see the GP about it?
 table(yr2004$anycd_ic, yr2004$contact_with_primary_care) %>% prop.table()
 
+# by age
+table(yr2004$chldage, yr2004$anycd_ic, yr2004$contact_with_primary_care) %>% prop.table()
+
+yr2004 %>%
+  count(chldage, anycd_ic, contact_with_primary_care) %>%
+  group_by(chldage) %>%
+  mutate(gp_bd_rate = n / sum(n),
+         subsample = sum(n)) %>%
+  filter(anycd_ic == "Disorder present") %>%
+  filter(contact_with_primary_care == 1) %>%
+  select(-anycd_ic, -contact_with_primary_care) %>%
+  mutate(lower = prop.test(n, subsample)$conf.int[1],
+         upper = prop.test(n, subsample)$conf.int[2])
+  
+
+# BD vs. ADHD contact rates with primary care
+yr2004 %>%
+  filter(anycd_ic == "Disorder present" | hyper_ic == "Disorder present") %>%
+  count(anycd_ic, hyper_ic, contact_with_primary_care) %>%
+  group_by(anycd_ic, hyper_ic) %>%
+  mutate(gp_rate = n / sum(n)) %>%
+  filter(contact_with_primary_care == 1)
+
 # contact with a specialist
 table(yr2004$anycd_ic, yr2004$contact_with_specialist) %>% prop.table(margin = 1)
 
